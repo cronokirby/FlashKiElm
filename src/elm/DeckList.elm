82454f67d62f70d042b2de7299fedb77aa91362f
cronokirby <cronokirby@gmail.com>
2017-01-21 15:22:50 +0100

    Seperates the DeckList view into its own file

---
+++ src/elm/DeckList.elm
@@ -0,0 +1,65 @@
+module DeckList exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+{- Model -}
+type alias Deck = { name : String, language : String }
+
+type alias Model = { deckInput : Deck
+                   , deckList : List Deck }
+
+init : Model
+init = Model (Deck "" "") []
+
+
+{- Update -}
+
+type Msg = NameInput String
+         | LangInput String
+         | Add
+
+update : Msg -> Model -> Model
+update msg model = case msg of
+    NameInput string ->
+        let oldDeck = model.deckInput
+        in ( { model | deckInput = { oldDeck | name = string } } )
+    LangInput string ->
+        let oldDeck = model.deckInput
+        in ( { model | deckInput = { oldDeck | language = string } } )
+    Add ->
+        ( { model |
+            deckList = model.deckInput :: model.deckList,
+            deckInput = Deck "" "" } )
+
+
+{- View -}
+
+-- The standard view, presenting a list of the current decks
+view : Model -> Html Msg
+view model =
+    div []
+      [ input [ placeholder "Deck name"
+              , value model.deckInput.name
+              , onInput NameInput ] []
+      , input [ placeholder "Deck language"
+              , value model.deckInput.language
+              , onInput LangInput ] []
+      , button [ onClick Add ] [ text "add Deck" ]
+      , div [] (List.map viewDeck model.deckList)
+      ]
+
+viewDeck : Deck -> Html Msg
+viewDeck {name, language} =
+    div []
+      [ table []
+        [ thead []
+          [ tr []
+            [ th [] [ text "Name" ]
+            , th [] [ text "Language" ]
+            ]
+          ]
+        , tbody [] (List.map (\n -> td [] [span [] [text n]]) [name, language])
+        ]
+      ]
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,11 +1,9 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
-import List exposing (..)
-
+import DeckList as DeckList exposing (..)
 
 main : Program Never Model Msg
 main = Html.program { init = init
@@ -17,79 +15,38 @@ main = Html.program { init = init
 {- Model -}
 
 
-type alias Deck = { name : String, language : String }
-
-type alias Model = { deckInput : Deck
-                   , deckList : List Deck
+type alias Model = { deckList : DeckList.Model
                    , currentView : ModelView }
 
 type ModelView = ModelView (Model -> Html Msg)
 
 init : (Model, Cmd Msg)
-init = (Model (Deck "" "") [] (ModelView deckListView), Cmd.none)
+init =
+    let deckList = DeckList.init
+    in (Model deckList (ModelView deckListView), Cmd.none)
 
 
 {- Update -}
 
-type Msg = NameInput String
-         | LangInput String
-         | Add
+type Msg = DeckList DeckList.Msg
          | ChangeView (Model -> Html Msg)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of
-    NameInput string ->
-        let oldDeck = model.deckInput
-        in ( { model | deckInput = { oldDeck | name = string } }
-           , Cmd.none)
-    LangInput string ->
-        let oldDeck = model.deckInput
-        in ( { model | deckInput = { oldDeck | language = string } }
-           , Cmd.none)
-
-    Add ->
-        ( { model |
-            deckList = model.deckInput :: model.deckList,
-            deckInput = Deck "" "" }
-        , Cmd.none )
-
+    DeckList msg ->
+        let deckList = DeckList.update msg model.deckList
+        in ( { model | deckList = deckList }, Cmd.none )
     ChangeView newView ->
         ( { model | currentView = ModelView newView }, Cmd.none )
 
 {- View -}
 
--- The standard view, presenting a list of the current decks
-deckListView : Model -> Html Msg
-deckListView model =
-    div []
-      [ input [ placeholder "Deck name"
-              , value model.deckInput.name
-              , onInput NameInput ] []
-      , input [ placeholder "Deck language"
-              , value model.deckInput.language
-              , onInput LangInput ] []
-      , button [ onClick Add ] [ text "add Deck" ]
-      , div [] (List.map viewDeck model.deckList)
-      ]
-
-viewDeck : Deck -> Html Msg
-viewDeck {name, language} =
-    div []
-      [ table []
-        [ thead []
-          [ tr []
-            [ th [] [ text "Name" ]
-            , th [] [ text "Language" ]
-            ]
-          ]
-        , tbody [] (List.map (\n -> td [] [span [] [text n]]) [name, language])
-        ]
-      ]
-
-
 helloView : Model -> Html Msg
 helloView _ = text "hello world"
+
+deckListView : Model -> Html Msg
+deckListView model = Html.map DeckList (DeckList.view model.deckList)
 
 -- Chosen to be able to switch views with a button
 view : Model -> Html Msg
@@ -97,8 +54,9 @@ view model = case model.currentView of
     ModelView currentView ->
       div []
         [ button [ onClick (ChangeView helloView) ] [ text "Hello View" ]
-        , button [ onClick (ChangeView deckListView) ] [ text "Deck View" ]
-        , currentView model ]
+        , button [ onClick (ChangeView deckListView) ]
+                 [ text "Deck View" ]
+        , (currentView model) ]
 
 
 subscriptions : Model -> Sub Msg
